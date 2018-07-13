a870e7fed0f24434ab733b0cb7ab1f88352265ff
DESKTOP-9R97706\Dev-02 <leefrances@me.com>
2018-07-13 11:22:14 +0900

    7/13 .Able to debug now .ImageUploader Component is gone all of a sudden

--- src/aura/ImageUploader/ImageUploaderController.js
+++ src/aura/ImageUploader/ImageUploaderController.js
@@ -1,5 +1,6 @@
 ({
 	handleUploadFinished: function (component, event) {
+		console.log('now we are in js controller');
         // Get the list of uploaded files
         var uploadedFiles = event.getParam("files");
         //set action to call updatePicturePath method from Server-side controller
@@ -9,12 +10,18 @@
         });
         action.setCallback(this, function(a){
         	if(a.getState() === "SUCCESS"){
+        		alert('success! hooray!');
             	var resultToast = $A.get("e.force:showToast");
         		resultToast.setParams({
                             "title": "Success!",
                             "message": uploadedFiles.length + "file uploaded successfully."
                         });
+                        console.log('resultToast: ' + resultToast);
         		resultToast.fire();;
+        	}else{ //대리님이 알려주신 에러 디버깅하는 방법 
+        		console.log('state : ' + a.getState());
+        		var error = a.getError();
+        		console.log('error : ' + error[0].message);
         	}
         });
     	$A.enqueueAction(action);
